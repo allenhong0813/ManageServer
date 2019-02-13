65b4b410f64d9c1a669b8a2e969ac28eb7aafa54
Allen <allenwang@shinewave.com>
2019-02-13 13:35:19 +0800

    LDAP authentication resolved

--- ManageServer/src/ManageServer/Program.cs
+++ ManageServer/src/ManageServer/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using ManageServer.Models;
 using Microsoft.Extensions.Logging;
+using Novell.Directory.Ldap;
 
 namespace ManageServer
 {
@@ -39,7 +40,35 @@ namespace ManageServer
             //    }
             //}
 
+            var User = new { username = "allen", password = "Shine@0813" };
+            //var User = new { username = "password", password = "password" };
+            var result = SelfLdapAuth(User.username, User.password);
+            Console.WriteLine($"LDAP Auth Result: {result}");
+
+
+
             host.Run();
+        }
+
+        public static bool SelfLdapAuth(string username, string password)
+        {
+            //var Host = "172.21.50.254";
+            //var BaseDC = "cn=users,dc=pbg";
+            //var Host = "dap.forumsys.com";
+            //var BaseDC = "cn=read-only-admin,dc=example,dc=com";
+            try
+            {
+                using (var ldapConn = new LdapConnection())
+                {
+                    ldapConn.Connect(Host, LdapConnection.DEFAULT_PORT);
+                    ldapConn.Bind($"uid={username},{BaseDC}", password);
+                    return ldapConn.Bound;
+                }
+            }
+            catch (LdapException ldapex)
+            {
+                throw ldapex;
+            }
         }
     }
 }
