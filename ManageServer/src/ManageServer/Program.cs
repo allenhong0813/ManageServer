c386530d5f79f064b917ef34772e6183134fdda7
Allen <allenwang@shinewave.com>
2019-02-14 18:05:32 +0800

    Login Page UI is OK

--- ManageServer/src/ManageServer/Program.cs
+++ ManageServer/src/ManageServer/Program.cs
@@ -21,54 +21,8 @@ namespace ManageServer
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
-
-            //初始化SeedData
-            //using (var scope = host.Services.CreateScope())
-            //{
-            //    var services = scope.ServiceProvider;
-
-            //    try
-            //    {
-            //        // Requires using ManageServer.Models;
-            //        SeedData.Initialize(services);
-            //    }
-            //    catch (Exception ex)
-            //    {
-
-            //        var logger = services.GetRequiredService<ILogger<Program>>();
-            //        logger.LogError(default(EventId), ex, "createSeedError");
-            //    }
-            //}
-
-            //var User = new { username = "allen", password = "Shine@0813" };
-            ////var User = new { username = "password", password = "password" };
-            //var result = SelfLdapAuth(User.username, User.password);
-            //Console.WriteLine($"LDAP Auth Result: {result}");
-
-
-
+            
             host.Run();
-        }
-
-        public static bool SelfLdapAuth(string username, string password)
-        {
-            var Host = "172.21.50.254";
-            var BaseDC = "cn=users,dc=pbg";
-            //var Host = "dap.forumsys.com";
-            //var BaseDC = "cn=read-only-admin,dc=example,dc=com";
-            try
-            {
-                using (var ldapConn = new LdapConnection())
-                {
-                    ldapConn.Connect(Host, LdapConnection.DEFAULT_PORT);
-                    ldapConn.Bind($"uid={username},{BaseDC}", password);
-                    return ldapConn.Bound;
-                }
-            }
-            catch (LdapException ldapex)
-            {
-                throw ldapex;
-            }
         }
     }
 }
